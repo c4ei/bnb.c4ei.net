6151eba0957abe2aacac6b1da117cafdb19d70ff
c4ei <c4ei.net@gmail.com>
2022-11-30 10:49:15 +0900

    ing

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,34 @@
+export default {
+  sawon: {
+    56: '0x52D1eC2ed762D7Fb3bE336Ec678569C730B87d66',
+    97: '',
+  },
+  masterChef: {
+    56: '0x55aC08Ccc202057f3a6B5098285c9C95da3338bD',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x5804BbBf35E59c14C3cC6FAA31925CE30f6441c4',
+    97: '',
+  },
+  busd: {
+    56: '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56',
+    97: '',
+  },
+  wcake: {
+    56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    97: '',
+  }
+}
---
+++ src/config/constants/farms.ts
@@ -0,0 +1,41 @@
+import contracts from './contracts'
+import { FarmConfig, QuoteToken } from './types'
+
+const farms: FarmConfig[] = [
+  {
+    pid: 0,
+    risk: 1,
+    lpSymbol: 'SAWON-BUSD LP',
+    lpAddresses: { 137: '0x89e485E5c252A3289CB6BFF07FbBDeEf8dEE0372', }, // SAWON-BUSD LP swap.c4ei.net 137
+    tokenSymbol: 'SAWON',
+    tokenAddresses: { 137: '0x52D1eC2ed762D7Fb3bE336Ec678569C730B87d66', }, // SaWonToken
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+    image: 'sawon-busd',
+  },
+  {
+    pid: 4, // 2022-11-18
+    risk: 2,
+    lpSymbol: 'SAWON-wMATIC LP',
+    lpAddresses: { 137: '0x91BE7d2884Ec195107598D607105Be9dc2265E7B', }, // SAWON-MATIC LP swap.c4ei.net 137
+    tokenSymbol: 'SAWON',
+    tokenAddresses: { 137: '0x52D1eC2ed762D7Fb3bE336Ec678569C730B87d66', }, // SaWonToken
+    quoteTokenSymbol: QuoteToken.MATIC,
+    quoteTokenAdresses: contracts.wbnb,
+    image: 'sawon-matic',
+    // isDisplay: 'N',
+  },
+  {
+    pid: 5,
+    risk: 1,
+    lpSymbol: 'wMATIC-BUSD LP',
+    lpAddresses: { 137: '0xF508DfA40709Fd456A136B47cB5ab363AD039f7f', },
+    tokenSymbol: 'MATIC',
+    tokenAddresses: { 137: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+    // isDisplay: 'N',
+  },
+]
+
+export default farms
