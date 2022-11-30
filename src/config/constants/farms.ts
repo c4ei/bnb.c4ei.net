db868647deb33784ec29ace95f51e5e96f6a3988
c4ei <c4ei.net@gmail.com>
2022-11-30 14:46:33 +0900

    ing

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -3,7 +3,7 @@ import { FarmConfig, QuoteToken } from './types'
 
 const farms: FarmConfig[] = [
   {
-    pid: 0,
+    pid: 1186131,
     risk: 1,
     lpSymbol: 'SAWON-BUSD LP',
     lpAddresses: { 56: '0xbAf32Aa941b469C433F5990022f2010B40A0124D', }, // SAWON-BUSD LP swap.c4ei.net 56
@@ -14,7 +14,7 @@ const farms: FarmConfig[] = [
     image: 'sawon-busd',
   },
   {
-    pid: 4, // 2022-11-18
+    pid: 1186131, // 2022-11-18
     risk: 2,
     lpSymbol: 'SAWON-wBNB LP',
     lpAddresses: { 56: '0x6a48d37EC1257EdF54b99b99B4E9E3bcbA598AE5', }, // SAWON-BNB LP swap.c4ei.net 56
@@ -26,7 +26,7 @@ const farms: FarmConfig[] = [
     // isDisplay: 'N',
   },
   {
-    pid: 5,
+    pid: 2,
     risk: 1,
     lpSymbol: 'wBNB-BUSD LP',
     lpAddresses: { 56: '0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16', },
